c704fef63f19e95cee62606b3a3852b9cf4ba860
mahajansachinr <mahajansachinr@gmail.com>
2024-01-14 22:20:52 +0530

    [Sachin] : i have added my first code for review...

---
+++ src/gitHubPractice/GitHubEg1.java
@@ -0,0 +1,16 @@
+package gitHubPractice;
+
+public class GitHubEg1 {
+
+	public static void main(String[] args) {
+
+		int a=10;
+		int b=20;
+		int c;
+		c=a+b;
+		System.out.println("Addition is :- "+ c);
+		
+		
+	}
+
+}
